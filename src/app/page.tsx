ca7c19653a1072acc8c4686e4904a5540ea9a2f5
misstitank3 <tianna.m.mccoy@gmail.com>
2024-03-29 16:18:49 -0500

    stuffs

---
+++ src/app/page.tsx
@@ -0,0 +1,50 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <h1>Meet Tianna McCoy: The Titan of Tech</h1>
+        <p>
+          Embark on the epic saga of Tianna McCoy, where each line of code tells a tale of valor, each project is a
+          battle won, and coffee is the elixir of life. Once a valiant Marine, Tianna traded her armor for algorithms,
+          becoming the tech world&apos;s answer to a mythic hero. She&apos;s not just writing code; she&apos;s
+          inscribing the runes of the future.
+        </p>
+
+        <h2>From Battlefields to Bug Fixes</h2>
+        <p>
+          With a journey spanning from the disciplined ranks of the Marine Corps to the dynamic realms of Homee Inc.,
+          Tianna&apos;s story is one for the ages. She commands the digital domain with the strategy of a general and
+          the grace of a diplomat. Leading her keyboard warriors into the fray, she emerges victorious, project after
+          project.
+        </p>
+
+        <h2>Her Arsenal</h2>
+        <p>
+          In the vast kingdom of technology, Tianna&apos;s arsenal is legendary. With HTML/CSS as her shield, JavaScript
+          her sword, and a quiver filled with React, Node.js, and Python, she faces down challenges with the ease of a
+          hero destined for greatness. MongoDB and Supabase are her loyal steeds, carrying her swiftly towards victory.
+        </p>
+
+        <h2>The Heart of a Mentor</h2>
+        <p>
+          But what truly sets Tianna apart in the tech arena isn&apos;t just her prowess in battle; it&apos;s her spirit
+          of mentorship. Like a seasoned sage, she guides fledgling developers through the forests of functions and
+          valleys of variables, ensuring no one is left behind. Her code doesn&apos;t just run; it inspires.
+        </p>
+
+        <p>
+          If you&apos;re in search of a champion to lead your tech quests, to imbue your projects with the strength of
+          her wisdom, or simply to claim victory in the coliseum of coding challenges, Tianna McCoy stands ready. In the
+          annals of software development, she&apos;s not just a participant; she&apos;s a legend. Rally to her banner
+          and let the adventures begin!
+        </p>
+        <h6>
+          For the long version of this story, check out the portfolio -&gt;{' '}
+          <a target="_blank" href="https://www.misstitank3.dev">
+            www.misstitank3.dev
+          </a>
+        </h6>
+      </div>
+    </main>
+  );
+}
