c6d311fb9a77923e7e6c9074b185afe809efd997
misstitank3 <tianna.m.mccoy@gmail.com>
2024-03-30 00:22:06 -0500

    mobile update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
       <div>
         <h1>Meet Tianna McCoy: The Titan of Tech</h1>
         <p>
